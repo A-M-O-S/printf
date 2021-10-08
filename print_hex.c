281353baec5ce97b788926b68896658359aac469
A-M-O-S <88572088+A-M-O-S@users.noreply.github.com>
2021-10-08 10:42:19 +0300

    update

--- print_hex.c
+++ print_hex.c
@@ -1,130 +1,73 @@
 #include "main.h"
-
 /**
- * print_hex - prints an unsigned int in hexidecimal form
- * @n: unsigned int to print
- * @c: flag to determine case of printing (0 = lower, 1 = upper)
- *
+ * printhex - prints a hexadecimal
+ * @format: format to print hexadecimal
+ * @pa: list that contains the hexadecimal
+ * Return: number.
+ */
+int printhex(char *format, va_list pa)
+{
+	unsigned int num = va_arg(pa, unsigned int);
+	unsigned int num2;
+	int i, i2, copy, contame = 0;
+	char *numhex;
+
+	(void)format;
+
+	if (num == 0)
+		return (_putchar('0'));
+	for (num2 = num; num2 != 0; contame++)
+	{
+		num2 = num2 / 16;
+	}
+	numhex = malloc(contame);
+	for (i = 0; num != 0; i++)
+	{
+		copy = num % 16;
+		if (copy < 10)
+			numhex[i] = copy + '0';
+		else
+			numhex[i] = copy - 10  + 'a';
+		num = num / 16;
+	}	
+	for (i2 = i - 1; i2 >= 0; i2--)
+		_putchar(numhex[i2]);
+	free(numhex);
+	return (contame);
+}
+/**
+ * printHEX - prints a hexadecimal
+ * @format: format to print hexadecimal
+ * @pa: va_list that contains the hexadecimal to print
  * Return: number of digits printed
  */
-int print_hex(unsigned int n, unsigned int c)
+int printHEX(char *format, va_list pa)
 {
-	unsigned int a[8];
-	unsigned int i, m, sum;
-	char diff;
-	int count;
+	unsigned int NUM = va_arg(pa, unsigned int);
+	unsigned int NUM2;
+	int I, I2, COPY, CONTAME = 0;
+	char *NUMHEX;
 
-	m = 268435456; /* (16 ^ 7) */
-	if (c)
-		diff = 'A' - ':';
-	else
-		diff = 'a' - ':';
-	a[0] = n / m;
-	for (i = 1; i < 8; i++)
-	{
-		m /= 16;
-		a[i] = (n / m) % 16;
-	}
-	for (i = 0, sum = 0, count = 0; i < 8; i++)
-	{
-		sum += a[i];
-		if (sum || i == 7)
-		{
-			if (a[i] < 10)
-				_putchar('0' + a[i]);
-			else
-				_putchar('0' + diff + a[i]);
-			count++;
-		}
-	}
-	return (count);
-}
-/**
- * print_x - takes an unsigned int and prints it in lowercase hex notation
- * @x: unsigned int to print
- *
- * Return: number of digits printed
- */
-int print_x(va_list x)
-{
-	return (print_hex(va_arg(x, unsigned int), 0));
-}
+	(void)format;
 
-/**
- * print_X - takes am unsigned int and prints it in uppercase hex notation
- * @X: unsigned int to print
- *
- * Return: number of digits printed
- */
-int print_X(va_list X)
-{
-	return (print_hex(va_arg(X, unsigned int), 1));
-}
-
-/**
- * _pow - calculates an exponent
- * @base: base of exponent
- * @exponent: exponent of number
- *
- * Return: base ^ exponent
- */
-static unsigned long _pow(unsigned int base, unsigned int exponent)
-{
-	unsigned int i;
-	unsigned long ans = base;
-
-	for (i = 1; i < exponent; i++)
+	if (NUM == 0)
+		return (_putchar('0'));
+	for (NUM2 = NUM; NUM2 != 0; CONTAME++)
 	{
-		ans *= base;
+		NUM2 = NUM2 / 16;
 	}
-	return (ans);
-}
-
-/**
- * print_p - prints an address
- * @p: address to print
- *
- * Return: number of characters to print
- */
-int print_p(va_list p)
-{
-	int count = 0;
-	unsigned int a[16];
-	unsigned int i, sum;
-	unsigned long n, m;
-	char *str = "(nil)";
-
-	n = va_arg(p, unsigned long);
-	if (n == 0)
+	NUMHEX = malloc(CONTAME);
+	for (I = 0; NUM != 0; I++)
 	{
-		for (i = 0; str[i]; i++)
-		{
-			_putchar(str[i]);
-			count++;
-		}
-		return (count);
+		COPY = NUM % 16;
+		if (COPY < 10)
+			NUMHEX[I] = COPY + '0';
+		else
+			NUMHEX[I] = COPY - 10 + 'A';
+		NUM = NUM / 16;
 	}
-	_putchar('0');
-	_putchar('x');
-	count = 2;
-	m = _pow(16, 15); /* 16 ^ 15 */
-	a[0] = n / m;
-	for (i = 1; i < 16; i++)
-	{
-		m /= 16;
-		a[i] = (n / m) % 16;
-	}
-	for (i = 0, sum = 0; i < 16; i++)
-	{
-		sum += a[i];
-		if (sum || i == 15)
-		{
-			if (a[i] < 10)
-				_putchar('0' + a[i]);
-			else
-				_putchar('0' + ('a' - ':') + a[i]);
-			count++;
-		}
-	}
-	return (count);
+	for (I2 = I - 1; I2 >= 0; I2--)
+		_putchar(NUMHEX[I2]);
+	free(NUMHEX);
+	return (CONTAME);
 }
